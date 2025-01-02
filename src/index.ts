bd7047fbd00dfb256d2261d6d6f3b1b31a8603cf
Adith A Danthi <adith.danthi@gmail.com>
2025-01-02 16:51:05 +0530

    feat: setup local db with turso

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { type Bindings, connectDb } from "./db";
 
-const app = new Hono()
+const app = new Hono<{ Bindings: Bindings }>();
+app.use("/*", cors());
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.get("/urls", async (c) => {
+  const db = await connectDb(c.env);
+  try {
+    const { rows: urls } = await db.execute("SELECT * FROM urls;");
+    return c.json(urls);
+  } catch (error) {
+    return c.json({ error: "Internal Server Error" }, 500);
+  }
+});
+
+export default app;
