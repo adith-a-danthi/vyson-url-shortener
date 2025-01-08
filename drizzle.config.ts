b683051d5a33520e37ff3222013e591eff304019
Adith A Danthi <adith.danthi@gmail.com>
2025-01-08 20:04:57 +0530

    feat: update drizzle config to change migration output path and add tables filter

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,11 +4,12 @@ import { defineConfig } from "drizzle-kit";
 config({ path: ".dev.vars" });
 
 export default defineConfig({
-  out: "./migrations",
+  out: "./.drizzle/migrations",
   schema: "./src/db/schema.ts",
   dialect: "turso",
   dbCredentials: {
     url: process.env.TURSO_DB_URL!,
     authToken: process.env.TURSO_DB_AUTH_TOKEN,
   },
+  tablesFilter: ["urls"],
 });
